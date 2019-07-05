085bfb0e71f7231c85d66175b577b6b61e0b7490
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2019-07-05 11:09:48 +0300

    Done

--- TakeFilesAdresses/Form1.Designer.cs
+++ TakeFilesAdresses/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SelectRootFolderBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectRootFolderBtn
+            // 
+            this.SelectRootFolderBtn.Location = new System.Drawing.Point(174, 222);
+            this.SelectRootFolderBtn.Name = "SelectRootFolderBtn";
+            this.SelectRootFolderBtn.Size = new System.Drawing.Size(165, 23);
+            this.SelectRootFolderBtn.TabIndex = 0;
+            this.SelectRootFolderBtn.Text = "Выбор корневой папки";
+            this.SelectRootFolderBtn.UseVisualStyleBackColor = true;
+            this.SelectRootFolderBtn.Click += new System.EventHandler(this.SelectRootFolderBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(540, 139);
+            this.textBox1.TabIndex = 1;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(454, 222);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(165, 23);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SelectRootFolderBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SelectRootFolderBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button StartBtn;
     }
 }
 
--- TakeFilesAdresses/Form1.cs
+++ TakeFilesAdresses/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,27 @@ namespace TakeFilesAdresses
 {
     public partial class Form1 : Form
     {
+        string rootFolderPath;
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void SelectRootFolderBtn_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                dialog.ShowDialog();
+                textBox1.Text += "Выбрана корневая папка: " + dialog.SelectedPath;
+                rootFolderPath = dialog.SelectedPath;
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            string[] filesNamesArr = Directory.GetFiles(rootFolderPath, "*.htm", SearchOption.AllDirectories);
+            File.WriteAllLines(rootFolderPath + "fileNameAdresses.txt", filesNamesArr);
         }
     }
 }
